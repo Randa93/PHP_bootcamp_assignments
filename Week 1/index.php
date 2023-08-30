89665c9d938397a02db57f6d24f91028eab45fc7
Randa Bassam Abou-Kersh <ran9dariri@gmail.com>
2023-08-30 16:07:46 +0300

    Add files via upload

---
+++ Week 1/index.php
@@ -0,0 +1,30 @@
+//====== 1 =========
+<?php
+/*
+multiple line comment
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?= "UTF-8";?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?="my fist php page"; ?></title>
+</head>
+<body>
+    <div><?php Echo "We Love"; ?></div>
+    <div><?php print "Elzero Channel"; ?></div>
+</body>
+</html>
+//====== 2 =========
+<?php
+//echo 'Prevent Me From Running Please';
+#echo 'Prevent Me From Running Please';
+/*echo 'Prevent Me From Running Please';*/
+?>
+//====== 3 =========
+<!-- My Application
+Version 1.0
+Created By Elzero -->
+ //====== 4 =========
+ //All works
